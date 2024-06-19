ea015116c92f6f969f0bc92dc1872d0c2b20bf1f
r4pidstart <r4pidstart@gmail.com>
2024-06-19 18:28:15 +0900

    bug: NULL-53 changed  get_user_query parameter to pydantic object

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ from pymilvus import MilvusClient
 from ai import query_analyzer as qa
 from ai import regex_generator as rg
 from ai import tag_finder as tf
-from ai import similarity_search as ss
+# from ai import similarity_search as ss
 
 app = FastAPI()
 
-def get_db() -> MilvusClient:
-    return connection.db_client
+# def get_db() -> MilvusClient:
+#     return connection.db_client
 
 @app.get("/")
 async def default():
     return "yes. it works."
 
-class User_Query:
+class User_Query(BaseModel):
     content: str
 
-@app.get("/user_query")
+@app.get("/user_query/")
 async def get_user_query(user_query: User_Query):
     query=User_Query.content
     query_type=qa.query_analyzer(query)
@@ -30,8 +30,8 @@ async def get_user_query(user_query: User_Query):
         return_content=rg.get_regex(query)
     elif query_type == qa.Query_Type.find_tag:
         return_content=tf.find_tag_id(query)
-    elif query_type == qa.Query_Type.similarity_search:
-        return_content=ss.search_similar_memos(query)
+    # elif query_type == qa.Query_Type.similarity_search:
+    #     return_content=ss.search_similar_memos(query)
 
     return {
         "type": query_type,
