2547eb4cc7a9e7a7884bddd24dda6565541f3791
r4pidstart <r4pidstart@gmail.com>
2024-06-19 16:53:12 +0900

    feat: NULL-74 connect to database

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from database import connection
+from pymilvus import MilvusClient
 
 app = FastAPI()
+
+def get_db() -> MilvusClient:
+    return connection.db_client
 
 @app.get("/")
 async def default():
     return "yes. it works."
+
