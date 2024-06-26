71b2e250e3dff10803d54a6c36940fb53fc3f846
r4pidstart <r4pidstart@gmail.com>
2024-06-27 08:06:17 +0900

    NULL-113 added test code

---
+++ drop_all_collections.py
@@ -0,0 +1,30 @@
+from langchain_milvus import Milvus
+from langchain_openai import OpenAIEmbeddings
+import database.connection as connection
+
+MEMO_NAME="memos"
+TAG_NAME="tags"
+
+embeddings=OpenAIEmbeddings(model="text-embedding-3-small")
+
+memo_store=Milvus(
+    embedding_function=embeddings,
+    collection_name=MEMO_NAME,
+    connection_args={
+        "uri": connection.MILVUS_URI,
+        # "db_name": connection.DB_NAME,
+    },
+    auto_id=True,
+    drop_old=True,
+)
+
+tag_store=Milvus(
+    embedding_function=embeddings,
+    collection_name=TAG_NAME,
+    connection_args={
+        "uri": connection.MILVUS_URI,
+        # "db_name": connection.DB_NAME,
+    },
+    auto_id=True,
+    drop_old=True,
+) 
