daa6f5aa64b2d73930751f43de69629edfb2dfcb
r4pidstart <r4pidstart@gmail.com>
2024-07-04 12:33:40 +0900

    modified: RG 리턴타입 변경

--- ai/for_search/regex_generator.py
+++ ai/for_search/regex_generator.py
@@ -6,7 +6,7 @@ from logger import logger as lg
 
 load_dotenv()
 
-def get_regex(q: str, country: str="korea") -> list[str]:
+def get_regex(q: str, country: str="korea") -> str:
     res=openai.chat.completions.create(
         model="gpt-4o",
         messages=[
@@ -35,4 +35,4 @@ def get_regex(q: str, country: str="korea") -> list[str]:
     except:
         raise Exception("Failed to get regex. result:", ret)
     
-    return [ret]
+    return ret
