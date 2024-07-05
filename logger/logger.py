f3d1d8c0c811285758adf1858db546bf42dea1a1
r4pidstart <r4pidstart@gmail.com>
2024-07-05 15:30:07 +0900

    modified: remove sentry_dsn on code

--- logger/logger.py
+++
@@ -1,7 +0,0 @@
-import sentry_sdk
-
-sentry_sdk.init(
-    dsn="https://3ba26ee363edade36c762dfa12c48718@o4507547686666240.ingest.de.sentry.io/4507547688042576",
-    traces_sample_rate=1.0,
-    profiles_sample_rate=1.0,
-)
