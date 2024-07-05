3034fc297b4618b78016ecabd263df22e426707e
r4pidstart <r4pidstart@gmail.com>
2024-07-05 15:06:34 +0900

    added: sentry_sdk

--- logger/logger.py
+++ logger/logger.py
@@ -1,23 +1,7 @@
-import logging, logging.handlers
-import datetime
-import uvicorn.logging
-import os
+import sentry_sdk
 
-def createDirectory(directory):
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    except OSError:
-        print("Error: Failed to create the directory.")
-
-createDirectory("./logs/debug")
-createDirectory("./logs/access")
-
-logger = logging.getLogger('uvicorn')
-console_formatter = uvicorn.logging.ColourizedFormatter(
-    "{asctime} - {message}",
-    style="{", use_colors=True)
-now = str(datetime.datetime.now()).split('.')[0].replace(' ', '_')
-handler = logging.handlers.TimedRotatingFileHandler(f'./logs/debug/code_exec_log_{now}.log', when='midnight', interval=1, backupCount=1)
-handler.setFormatter(console_formatter)
-logger.addHandler(handler)
+sentry_sdk.init(
+    dsn="https://3ba26ee363edade36c762dfa12c48718@o4507547686666240.ingest.de.sentry.io/4507547688042576",
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
