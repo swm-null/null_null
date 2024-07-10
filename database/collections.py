a7091508f046a06843efa88f75cb1f22d683e954
r4pidstart <r4pidstart@gmail.com>
2024-07-10 13:09:25 +0900

    modified: collections

--- database/collections.py
+++ database/collections.py
@@ -1,28 +1,33 @@
-from langchain_milvus import Milvus
+from langchain_mongodb import MongoDBAtlasVectorSearch
 from langchain_openai import OpenAIEmbeddings
 import database.connection as connection
 
-MEMO_NAME="memos"
-TAG_NAME="tags"
+MEMO_COLLECTION_NAME="memos"
+TAG_COLLECTION_NAME="tags"
+
+MEMO_CONTENT_NAME="content"
+TAG_CONTENT_NAME="name"
+
+MEMO_INDEX_NAME="vector_index_memos"
+TAG_INDEX_NAME="vector_index_tags"
 
 embeddings=OpenAIEmbeddings(model="text-embedding-3-small")
 
-memo_store=Milvus(
-    embedding_function=embeddings,
-    collection_name=MEMO_NAME,
-    connection_args={
-        "uri": connection.MILVUS_URI,
-        # "db_name": connection.DB_NAME,
-    },
-    auto_id=True,
+if connection.DB_NAME == None:
+    raise Exception("Invalid DB_NAME")
+
+memo_collection=connection.client[connection.DB_NAME][MEMO_COLLECTION_NAME]
+memo_store=MongoDBAtlasVectorSearch(
+    collection=memo_collection,
+    embedding=embeddings,
+    index_name=MEMO_INDEX_NAME,
+    text_key=MEMO_CONTENT_NAME,
 )
 
-tag_store=Milvus(
-    embedding_function=embeddings,
-    collection_name=TAG_NAME,
-    connection_args={
-        "uri": connection.MILVUS_URI,
-        # "db_name": connection.DB_NAME,
-    },
-    auto_id=True,
+tag_collection=connection.client[connection.DB_NAME][TAG_COLLECTION_NAME]
+tag_store=MongoDBAtlasVectorSearch(
+    collection=tag_collection,
+    embedding=embeddings,
+    index_name=TAG_INDEX_NAME,
+    text_key=TAG_CONTENT_NAME,
 )
