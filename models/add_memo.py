090f793792669ed7a26fa154495e386965279a6b
r4pidstart <r4pidstart@gmail.com>
2024-07-15 18:19:23 +0900

    modified: models for /add_memo

--- models/add_memo.py
+++ models/add_memo.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Arg_add_memo(BaseModel):
     content: str
 
-class Res_memo(BaseModel):
+class Res_memo_tag(BaseModel):
     name: str
     embedding: list[float]
+    parent: Optional[str]
     
 class Res_add_memo(BaseModel):
     memo_embeddings: list[float]
     existing_tag_ids: list[str]
-    new_tags: list[Res_memo]
+    new_tags: list[Res_memo_tag]
