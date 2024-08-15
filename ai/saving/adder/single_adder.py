9aae7968e9e59f31c6aff9ad4c7d04a4f0d864b5
r4pidstart <r4pidstart@gmail.com>
2024-08-15 23:15:05 +0900

    added: single and batch(stupid) adder

--- ai/saving/adder/single_adder.py
+++ ai/saving/adder/single_adder.py
@@ -1,13 +1,16 @@
 from datetime import datetime
+import logging
 from langchain_openai import OpenAIEmbeddings
-from ai.saving.tag import query_extractor as qe
-from ai.saving.utils.embedder import embedder
+from ai.saving import query_extractor as qe
+from ai.saving.tag.single import get_tag_single
+from ai.utils.embedder import embedder
 from models.add_memo import Arg_add_memo, Res_add_memo, Res_memo_tag
+from models.memos import *
 
 embeddings=OpenAIEmbeddings(model="text-embedding-3-small")
 
-def single_adder(memo: Memos_raw_memo) -> Memos_processed_memo:
-    new_tag_list, parent_tags, dir_relations=get_tag_single(memo.content)
+def single_adder(memo: Memos_raw_memo, lang: str="Korean") -> Memos_processed_memo:
+    new_tag_list, parent_tags, dir_relations=get_tag_single(memo.content, lang)
     logging.info("[single_adder]\n## new_tag_list:\n%s\n\n## parent_tags:\n%s\n\n## dir_relations:\n%s\n\n", new_tag_list, parent_tags, dir_relations)
 
     new_tags: list[Memos_tag]=[
@@ -43,7 +46,6 @@ def single_adder_deprecated(memo: Arg_add_memo) -> Res_add_memo:
     existing_tag_ids, new_tags = qe.query_extractor(memo.content)
 
     return Res_add_memo(
-        memo_embeddings=embedder.embed_query(memo.content),
         existing_tag_ids=existing_tag_ids,
         new_tags=new_tags,
         timestamp=datetime.now() if memo.timestamp is None else memo.timestamp,
---
+++ models/memos.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class Memos_tag(BaseModel):
+    name: str
+    id: Optional[str]
+    embedding: list[float]
+
+class Memos_tag_relation(BaseModel):
+    parent_id: str
+    child_id: str
+
+class Memos_raw_memo(BaseModel):
+    content: str
+    timestamp: Optional[datetime]=None
+    
+class Memos_processed_memo(BaseModel):
+    timestamp: datetime
+    parent_tag_ids: list[str]=Field(description="relations between tag and memo")
+    tag_relations: list[Memos_tag_relation]=Field(description="relations between tags")
+    new_tags: list[Memos_tag]
+    embedding: list[float]
+
+class Arg_post_memos(BaseModel):
+    content: list[Memos_raw_memo]
+
+class Res_post_memos(BaseModel):
+    content: list[Memos_processed_memo]
