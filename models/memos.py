27591367002f572476c254898b867024e35f230b
r4pidstart <r4pidstart@gmail.com>
2024-09-02 00:36:29 +0900

    modified: some schemas

--- models/memos.py
+++ models/memos.py
@@ -11,6 +11,10 @@ class Memos_tag(BaseModel):
 class Memos_tag_relation(BaseModel):
     parent_id: Optional[str]
     child_id: str
+    
+class Memos_relations(BaseModel):
+    added: list[Memos_tag_relation]
+    deleted: list[Memos_tag_relation]
 
 class Memos_raw_memo(BaseModel):
     content: str
@@ -19,13 +23,13 @@ class Memos_raw_memo(BaseModel):
 class Memos_processed_memo(BaseModel):
     timestamp: datetime
     content: str
-    parent_tag_ids: list[str]=Field(description="relations between tag and memo")
-    tag_relations: list[Memos_tag_relation]=Field(description="relations between tags")
+    parent_tag_ids: list[str]=Field(description="relations between leaf tags and this memo")
+    tag_relations: Memos_relations=Field(description="relations between tags")
     new_tags: list[Memos_tag]
     embedding: list[float]
 
 class Arg_post_memos(BaseModel):
-    content: list[Memos_raw_memo]
+    memos: list[Memos_raw_memo]
 
 class Res_post_memos(BaseModel):
-    content: list[Memos_processed_memo]
+    processed_memos: list[Memos_processed_memo]
