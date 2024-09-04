5ffe1d9460b93bd3e0b00b01a4edb43193a5b21e
r4pidstart <r4pidstart@gmail.com>
2024-09-05 02:46:03 +0900

    modified: version information

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ from models.kakao_parser import *
 
 app = FastAPI(
     title="Oatnote AI",
-    description="after PR #45",
-    version="0.1.7",
+    description="after PR #47",
+    version="0.1.8",
 )
 init(app)
     
