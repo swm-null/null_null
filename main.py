0bc2be0e3485e9b77d2b1c06dfb09163a2aebb4e
Taesik Jo <67845112+r4pidstart@users.noreply.github.com>
2024-09-04 20:55:39 +0900

    NULL-330-hotfix fixed: prompt for tag extracting (#44)
    * fixed: prompt for tag extracting
    
    * modified: version and description

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ from models.kakao_parser import *
 
 app = FastAPI(
     title="Oatnote AI",
-    description="after PR #40, changed genarate tag algorithm",
-    version="0.1.4",
+    description="after PR #44, changed genarate tag algorithm",
+    version="0.1.6",
 )
 init(app)
     
