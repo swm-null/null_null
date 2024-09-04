61125e1b0bde3b195792d2f619a2af6cafad417e
r4pidstart <r4pidstart@gmail.com>
2024-09-05 02:30:39 +0900

    modiied: directories formatting method

--- ai/saving/tag/locator/get_formatted_directories.py
+++ ai/saving/tag/locator/get_formatted_directories.py
@@ -1,27 +1,51 @@
-import os
-import requests
+from ai.saving.utils.get_tag_relations_from_db import get_tag_relations_from_db
+from ai.database.collections.tag_store import tag_collection, TAG_UID_NAME, TAG_ID_NAME, TAG_CONTENT_NAME
 
 
-backend_url: str=str(os.getenv("BACKEND_API_URL"))
+def get_formatted_directories(user_id: str) -> str:
+    graph: dict[str, list[str]]=get_tag_relations_from_db(user_id)
+    tag_id_to_name: dict[str, str]=_get_tags_from_db(user_id)
+    return _format_graph(graph, tag_id_to_name)
 
-# TODO: parallelize this
-def get_formatted_directories(current: str="", depth: int=0) -> str:
-    current_directories: list[dict[str, str]]=_get_current_directories(current)
-    
+def _format_graph(graph: dict[str, list[str]], tag_id_to_name: dict[str, str], now: str="root", depth: int=0) -> str:
     result: str=""
-    for next in current_directories:
-        next_line: str="-"*depth
-        next_line+=f'{next["name"]} ({next["id"]})\n'
-        next_result=get_formatted_directories(next["id"], depth+1)
+    next_ids: list[str]=_get_next_ids_from_graph(graph, now)
+    
+    for next_id in next_ids:
+        next_line: str="-"*depth 
+        next_line+=f'{tag_id_to_name[next_id]} ({next_id})\n'
+        next_result=_format_graph(graph, tag_id_to_name, next_id, depth+1)
         if next_result != "":
             next_line+=next_result
-            
+        
         result+=next_line
-        
-    return result
-        
-def _get_current_directories(current: str) -> list[dict[str, str]]:
-    endpoint=backend_url+"/tags"+("/root" if current=="" else f'/{current}/childTags')
-    result=requests.get(endpoint).json()
     
     return result
+
+def _get_next_ids_from_graph(graph: dict[str, list[str]], now: str) -> list[str]:
+    if now=="root":
+        return _get_roots_from_graph(graph)
+    else:
+        return graph[now] if now in graph else []
+    
+def _get_roots_from_graph(graph: dict[str, list[str]]):
+    has_parent: dict[str, int]={}
+    
+    for parent_id in graph:
+        has_parent[parent_id]=0
+    
+    for parent_id in graph:
+        for child_id in graph[parent_id]:
+            has_parent[child_id]=1
+    
+    return [id for id, has_parent in has_parent.items() if has_parent]
+            
+def _get_tags_from_db(user_id: str) -> dict[str, str]:
+    tags: dict[str, str]={}
+    raw_tags=tag_collection.find({TAG_UID_NAME: user_id})
+    
+    for raw_tag in raw_tags:
+        tags[raw_tag[TAG_ID_NAME]]=raw_tag[TAG_CONTENT_NAME]
+    
+    return tags
+    
