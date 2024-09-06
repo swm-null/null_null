2fd9566e3041457e7ad3d50a4360d124167f8887
r4pidstart <r4pidstart@gmail.com>
2024-09-06 18:26:21 +0900

    modified: schema field name

--- ai/saving/structurer/structurer.py
+++ ai/saving/structurer/structurer.py
@@ -5,7 +5,7 @@ from ai.saving.utils.get_tag_relations_from_db import get_tag_relations_from_db
 def memos_structurer(processed_memos: list[Memos_processed_memo], user_id: str):
     graph: dict[str, list[str]]=get_tag_relations_from_db(user_id)
     for memo in processed_memos:
-        _structurer(graph, memo.tag_relations)
+        _structurer(graph, memo.tags_relations)
     
     return graph
 
