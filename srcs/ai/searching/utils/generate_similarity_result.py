f119cb70ba0195e135bf14545fe5daa0bfd30ff9
r4pidstart <r4pidstart@gmail.com>
2024-09-19 05:39:08 +0900

    added: validator for similarity result

--- srcs/ai/searching/utils/generate_similarity_result.py
+++ srcs/ai/searching/utils/generate_similarity_result.py
@@ -1,4 +1,6 @@
 import logging
+
+from fastapi import HTTPException
 from ai.searching.models.memo import Memo
 from ai.searching.utils.chains.generate_similarity_result_chain import generate_similarity_result_chain
 
@@ -6,6 +8,7 @@ def generate_similarity_result(query: str, memos: list[Memo], lang: str="Korean"
     formatted_memos: str=_format_memos(memos)
     generated_result=generate_similarity_result_chain.invoke({"query": query, "context": formatted_memos, "lang": lang})
     logging.info("[generate similarity result]\n## %s\n%s\n\n", generated_result.answer, generated_result.used_memo_ids)
+    _validate_result(memos, generated_result.used_memo_ids)
     
     return generated_result.answer, generated_result.used_memo_ids
     
@@ -17,3 +20,9 @@ def _format_memos(memos: list[Memo]) -> str:
         formatted_memos+=f"{memo.content}\n\n"
         
     return formatted_memos
+
+def _validate_result(memos: list[Memo], used_memo_ids: list[str]) -> None:
+    missing_ids = [used_memo_id for used_memo_id in used_memo_ids if not any(memo.id == used_memo_id for memo in memos)]
+    if missing_ids:
+        logging.error("[_validate_result]\n## %s\n\n", missing_ids)
+        raise HTTPException(status_code=500, headers={"/search._validate_result": str(missing_ids)})
