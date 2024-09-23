eb2d1068217df9fd213a2c6186d9e2a11f9bf977
r4pidstart <r4pidstart@gmail.com>
2024-09-23 10:33:03 +0900

    modified: deprecated previous memo(s) endpoints

--- srcs/main.py
+++ srcs/main.py
@@ -6,20 +6,14 @@ import logging
 from init import init
 from ai.utils import embedder
 from ai.searching import search
-from ai.saving import single_processor, batch_processor, memo_structurer, memos_structurer
-from ai.saving.parser import kakao_parser
+from ai.saving.tag import create_tag, create_tags
+from ai.saving.structure import process_memos, get_structure
 from models import *
-
-from ai.searching_deprecated.regex_generator import get_regex
-from ai.searching_deprecated.query_analyzer import Query_Type, query_analyzer
-from ai.searching_deprecated.similarity_search import similarity_search
-from ai.searching_deprecated.tag_finder import find_tag_ids
-
 
 app = FastAPI(
     title="Oatnote AI",
-    description="after PR #61, https://github.com/swm-null/null_null/pull/61",
-    version="0.2.6",
+    description="after PR #62, https://github.com/swm-null/null_null/pull/62",
+    version="0.2.7",
 )
 init(app)
     
@@ -30,24 +24,6 @@ async def default():
 @app.post("/search", response_model=Res_post_search)
 def post_search(body: Arg_post_search):
     return search(body.content, body.user_id)
-
-@app.post("/memos", response_model=Res_post_memos, status_code=status.HTTP_200_OK)
-def post_memos(body: Arg_post_memos):
-    processed_memos=asyncio.run(batch_processor(body.memos, body.user_id))
-    
-    return Res_post_memos(
-        processed_memos=processed_memos,
-        new_structure=memos_structurer(processed_memos, body.user_id)
-    )
-
-@app.post("/memo", response_model=Res_post_memo, status_code=status.HTTP_200_OK)
-def post_memo(body: Arg_post_memo):
-    processed_memo=single_processor(body.memo, body.user_id)
-    
-    return Res_post_memo(
-        processed_memo=processed_memo,
-        new_structure=memo_structurer(processed_memo, body.user_id)
-    )
     
 @app.post("/get-embedding", response_model=Res_get_embedding)
 def get_embedding(body: Arg_get_embedding):
@@ -55,23 +31,43 @@ def get_embedding(body: Arg_get_embedding):
         embedding=embedder.embed_query(body.content)
     )
 
-@app.post("/kakao-parser", response_model=Res_post_memos)
-def post_kakao_parser(body: Arg_kakao_parser):
-    parsed_memos: list[tuple[str, datetime]]=kakao_parser(body.content, body.type)
-    memos: list[Memos_raw_memo]=[
-        Memos_raw_memo(
-            content=content, 
-            timestamp=timestamp
-        ) for content, timestamp in parsed_memos
-    ]
+@app.post("/memo/tags", response_model=Res_post_memo_tags)
+def post_memo_tags(body: Body_post_memo_tags):
+    return Res_post_memo_tags(tags=asyncio.run(create_tags(body.user_id, body.raw_memos)))
+
+@app.post("/memo/tag", response_model=Res_post_memo_tag)
+def post_memo_tag(body: Body_post_memo_tag):
+    return Res_post_memo_tag(tag=asyncio.run(create_tag(body.user_id, body.raw_memo)))
     
-    return post_memos(Arg_post_memos(
-        user_id=body.user_id,
-        memos=memos
-    ))
+# TODO: add mutex
+@app.post("/memo/structures", response_model=Res_post_memo_structures)
+def post_memo_structures(body: Body_post_memo_structures):
+    processed_memos, relations, tags=asyncio.run(process_memos(body.user_id, body.memos))
+    structure: dict[str, list[str]]=get_structure(body.user_id, relations)
+
+    return Res_post_memo_structures(
+        processed_memos=processed_memos,
+        tags_relations=Memos_relations(added=relations, deleted=[]),
+        new_tags=tags,
+        new_structure=structure
+    )
+    
 
 if __name__ == '__main__':
     uvicorn.run(app)
+
+# deprecated imports
+from ai.searching_deprecated.regex_generator import get_regex
+from ai.searching_deprecated.query_analyzer import Query_Type, query_analyzer
+from ai.searching_deprecated.similarity_search import similarity_search
+from ai.searching_deprecated.tag_finder import find_tag_ids
+from ai.saving import single_processor, batch_processor
+from ai.saving.structurer_deprecated.structurer import memo_structurer, memos_structurer
+from ai.saving.parser import kakao_parser
+from models.memo import *
+from models.memos import *
+from models.search_deprecated import *
+from models.kakao_parser import *
 
 @app.post("/search_deprecated", response_model=Res_search, deprecated=True)
 def search_depreacted(body: Arg_search):
@@ -99,3 +95,36 @@ def search_depreacted(body: Arg_search):
     logging.info("[/search] query: %s / query type: %s \nreturn: %s", body.content, return_content.type, return_content)
 
     return return_content
+
+@app.post("/memos", response_model=Res_post_memos, status_code=status.HTTP_200_OK, deprecated=True)
+def post_memos(body: Arg_post_memos):
+    processed_memos=asyncio.run(batch_processor(body.memos, body.user_id))
+    
+    return Res_post_memos(
+        processed_memos=processed_memos,
+        new_structure=memos_structurer(processed_memos, body.user_id)
+    )
+
+@app.post("/memo", response_model=Res_post_memo, status_code=status.HTTP_200_OK, deprecated=True)
+def post_memo(body: Arg_post_memo):
+    processed_memo=single_processor(body.memo, body.user_id)
+    
+    return Res_post_memo(
+        processed_memo=processed_memo,
+        new_structure=memo_structurer(processed_memo, body.user_id)
+    )
+    
+@app.post("/kakao-parser", response_model=Res_post_memos, deprecated=True)
+def post_kakao_parser(body: Arg_kakao_parser):
+    parsed_memos: list[tuple[str, datetime]]=kakao_parser(body.content, body.type)
+    memos: list[Memos_raw_memo]=[
+        Memos_raw_memo(
+            content=content, 
+            timestamp=timestamp
+        ) for content, timestamp in parsed_memos
+    ]
+    
+    return post_memos(Arg_post_memos(
+        user_id=body.user_id,
+        memos=memos
+    ))
