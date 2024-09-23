1869c736fe0c30991e584fd5fde894aebf815cb0
r4pidstart <r4pidstart@gmail.com>
2024-09-23 11:13:43 +0900

    fixed: losing the id of the tag

--- srcs/ai/saving/structure/processor.py
+++ srcs/ai/saving/structure/processor.py
@@ -52,9 +52,10 @@ def _locate_memos(user_id: str, memos_and_tags: list[Memos_memo_and_tags], lang:
     
     relations, located_tags=locate_tags(user_id, new_tags, lang)
     located_and_merged_tags=_merge_located_tags_and_new_tags(located_tags, new_tags)
+    merged_relations=_merge_relations_and_new_tags(relations, new_tags)
     located_memos_and_tags: list[Memo]=_link_memos_and_tags(memos, located_and_merged_tags)
     
-    return located_memos_and_tags, relations, located_and_merged_tags
+    return located_memos_and_tags, merged_relations, located_and_merged_tags
         
 def _get_new_tags(tags: list[Tag]) -> list[Tag]:
     return [tag for tag in tags if tag.is_new]
@@ -71,6 +72,18 @@ def _merge_located_tags_and_new_tags(located_tags: list[Tag], new_tags: list[Tag
         ) for tag in located_tags
     ]
 
+def _merge_relations_and_new_tags(relations: list[Directory_relation], new_tags: list[Tag]) -> list[Directory_relation]:
+    tag_name_to_original_tag_id: dict[str, str]={tag.name: tag.id for tag in new_tags}
+    
+    return [
+        Directory_relation(
+            parent_id=tag_name_to_original_tag_id[relation.parent_name] if relation.parent_name in tag_name_to_original_tag_id else relation.parent_id,
+            parent_name=relation.parent_name,
+            child_id=tag_name_to_original_tag_id[relation.child_name] if relation.child_name in tag_name_to_original_tag_id else relation.child_id,
+            child_name=relation.child_name
+        ) for relation in relations
+    ]
+    
 def _link_memos_and_tags(memos: dict[int, Memo], tags: list[Tag]) -> list[Memo]:
     linked_memo_id_to_tags: dict[int, list[Tag]]=dict()
     
