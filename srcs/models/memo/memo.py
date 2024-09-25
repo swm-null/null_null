b648baa55800f8626fe6efeb75f4a223b65c6b3e
r4pidstart <r4pidstart@gmail.com>
2024-09-25 14:19:33 +0900

    modifed: removed embedding on /memo/structure body

--- srcs/models/memo/memo.py
+++ srcs/models/memo/memo.py
@@ -27,7 +27,7 @@ class Memo_tag(BaseModel):
 class Memo_memo_and_tags(BaseModel):
     content: str=Field(examples=["치즈라면 레시피: 라면 위에 치즈를 얹어 내놓는 음식으로 제조법 또한 간단하여 그냥 라면을 끓인 후 시중에 판매되는 슬라이스 체다치즈를 얹는 것으로 완성."])
     timestamp: Optional[datetime]=None
-    tags: list[Memo_tag] 
+    tags: list[Memo_tag_name_and_id] 
     
 class Memo_tag_relation(BaseModel):
     parent_id: str
