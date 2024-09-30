3b200f37181270594f5dd8178666a0c641e8efad
Taesik Jo <67845112+r4pidstart@users.noreply.github.com>
2024-09-30 19:05:00 +0900

    fixed: async tag extract (#64)

--- srcs/ai/saving/tag/utils/extractor/tag_extractor.py
+++ srcs/ai/saving/tag/utils/extractor/tag_extractor.py
@@ -1,12 +1,13 @@
+import asyncio
 import logging
 from langchain_core.runnables import RunnableParallel
 from ai.saving.tag.utils.extractor.chains import existing_tag_chain, new_tag_chain
 from ai.saving._models import Tag
 
 
-def extract_tags(query: str, user_id: str, lang: str="Korean") -> list[Tag]:
+async def extract_tags(query: str, user_id: str, lang: str="Korean") -> list[Tag]:
     chain=RunnableParallel(existing=existing_tag_chain, new=new_tag_chain)
-    chain_result=chain.invoke({"query": query, "lang": lang, "user_id": user_id})
+    chain_result=await asyncio.to_thread(chain.invoke, {"query": query, "lang": lang, "user_id": user_id})
     logging.info("[extract_tags]\n## existing tags:\n%s\n\n## new tags:\n%s\n\n", chain_result["existing"].tag_list, chain_result["new"])
     
     extracted_tag: list[Tag]=[]
