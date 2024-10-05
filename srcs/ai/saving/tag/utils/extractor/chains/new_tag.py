2f1305a7a1f54544feae03cec90302e258835bf0
r4pidstart <r4pidstart@gmail.com>
2024-10-05 12:51:56 +0900

    modified: optimize costs

--- srcs/ai/saving/tag/utils/extractor/chains/new_tag.py
+++ srcs/ai/saving/tag/utils/extractor/chains/new_tag.py
@@ -8,24 +8,24 @@ from langchain_core.prompts import PromptTemplate
 _parser = PydanticOutputParser(pydantic_object=Tag)
 
 _new_chain_prompt=PromptTemplate.from_template(
-    """
-    You're an expert at categorizing documents.
-    Given a document, you create a category to encompass it.
-    Create based on how the average person categorizes notes.
-    Not just a category for this document, but a category for anything similar to this document. 
-    But I don't want the category to be so broad that it could contain too many documents.
+"""
+You're an expert at categorizing documents.
+Given a document, you create a category to encompass it.
+Create based on how the average person categorizes notes.
+Not just a category for this document, but a category for anything similar to this document. 
+But I don't want the category to be so broad that it could contain too many documents.
 
-    I'm attaching a document for you.
-    Create the category in the user's language.
-    Categories can be one or two words.
-    The document is given between ᝃ. Sometimes it can be empty.
-    
-    User's language: {lang}
-    Document: ᝃ{query}ᝃ
+I'm attaching a document for you.
+Create the category in the user's language.
+Categories can be one or two words.
+The document is given between ᝃ. Sometimes it can be empty.
 
-    {format}
-    Set new category's id to category's name.
-    """,
+User's language: {lang}
+Document: ᝃ{query}ᝃ
+
+{format}
+Set new category's id to category's name.
+""",
     partial_variables={
         "format": _parser.get_format_instructions()
     }
--- srcs/ai/searching/utils/chains/generate_similarity_result_chain.py
+++ srcs/ai/searching/utils/chains/generate_similarity_result_chain.py
@@ -13,30 +13,30 @@ class _generate_similarity_result_chain_output(BaseModel):
 _parser = PydanticOutputParser(pydantic_object=_generate_similarity_result_chain_output)
 
 _generate_similarity_result_chain_prompt=PromptTemplate.from_template(
-    """
-    You need to answer user questions.
-    Answer in the user's language.
+"""
+You need to answer user questions.
+Answer in the user's language.
 
-    I'm attaching some pre-written notes from the user that might help you answer this question.
-    If a user asks a time-related question, consider the current time and the time the note was written.
+I'm attaching some pre-written notes from the user that might help you answer this question.
+If a user asks a time-related question, consider the current time and the time the note was written.
 
-    1. Determine if you can answer the user's question with the information provided.
+1. Determine if you can answer the user's question with the information provided.
 
-    2-1. If you can, create an answer to the user's question using the information provided and end this prompt. 
-    2-2. If you can't, print that you can't answer the user's question with the given notes. Then use the Internet to create and print an answer to the question and end this prompt.
+2-1. If you can, create an answer to the user's question using the information provided and end this prompt. 
+2-2. If you can't, print that you can't answer the user's question with the given notes. Then use the Internet to create and print an answer to the question and end this prompt.
 
-    Language: {lang}
-    Current time: {current_time}
-    The user's question: {query}
-    
-    Notes
-    [
-    {context}    
-    ]
+Language: {lang}
+Current time: {current_time}
+The user's question: {query}
 
-    {format}
-    If you use a memo to answer, write the ID of the memo in used_memo_ids.
-    """,
+Notes
+[
+{context}    
+]
+
+{format}
+If you use a memo to answer, write the ID of the memo in used_memo_ids.
+""",
     partial_variables={
         "format": _parser.get_format_instructions(),
         "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
