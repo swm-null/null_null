55f436a8f4a003e5b50d5fdad3730cdd04cc4243
r4pidstart <r4pidstart@gmail.com>
2024-10-06 02:11:00 +0900

    modified: removed debug prints

--- srcs/ai/saving/structure/utils/metadata_extractor/metadata_extractor.py
+++ srcs/ai/saving/structure/utils/metadata_extractor/metadata_extractor.py
@@ -13,7 +13,6 @@ async def _extract_and_assign_metadata(id: int, memo: Memo, lang: str) -> tuple[
     chain_result: metadata_extractor_chain_output=await metadata_extractor_chain.ainvoke({"content": memo.content, "lang": lang})
     image_to_texts_tasks=[asyncio.to_thread(image_to_text, image) for image in memo.image_urls]
     image_to_texts: list[str]=await asyncio.gather(*image_to_texts_tasks)
-    print(image_to_texts)
     
     return id, Memo(
         content=memo.content,
