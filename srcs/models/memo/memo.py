c6a4bd64b4c3a4345c0ce40cf9285657d3c9b810
r4pidstart <r4pidstart@gmail.com>
2024-10-06 02:08:22 +0900

    added: image_urls to rest models

--- srcs/models/memo/memo.py
+++ srcs/models/memo/memo.py
@@ -5,11 +5,13 @@ from pydantic import BaseModel, Field
 
 class Memo_raw_memo(BaseModel):
     content: str=Field(examples=["치즈라면 레시피: 라면 위에 치즈를 얹어 내놓는 음식으로 제조법 또한 간단하여 그냥 라면을 끓인 후 시중에 판매되는 슬라이스 체다치즈를 얹는 것으로 완성."])
+    images: list[str]=[]
     timestamp: Optional[datetime]=None
     
 class Memo_processed_memo(BaseModel):
     timestamp: datetime
     content: str
+    image_urls: list[str]
     metadata: str
     parent_tag_ids: list[str]=Field(description="relations between leaf tags and this memo")
     embedding: list[float]
@@ -27,6 +29,7 @@ class Memo_tag(BaseModel):
 
 class Memo_memo_and_tags(BaseModel):
     content: str=Field(examples=["치즈라면 레시피: 라면 위에 치즈를 얹어 내놓는 음식으로 제조법 또한 간단하여 그냥 라면을 끓인 후 시중에 판매되는 슬라이스 체다치즈를 얹는 것으로 완성."])
+    image_urls: list[str]=[]
     timestamp: Optional[datetime]=None
     tags: list[Memo_tag_name_and_id] 
     
