8bde630b4f0a27f95f93f74683dd0ff6acea1441
Taesik Jo <67845112+r4pidstart@users.noreply.github.com>
2024-10-07 16:24:47 +0900

    NULL-414 removed: deprecated (#72)
    * removed: deprecated
    
    * modified: version information
    
    * Merge branch 'develop' into NULL-414-remove-deprecated

--- srcs/ai/saving/__init__.py
+++
@@ -1,3 +0,0 @@
-from ai.saving.processor.single_processor import single_processor
-from ai.saving.processor.batch_processor import batch_processor
-from ai.saving.structurer_deprecated.structurer import memo_structurer, memos_structurer
