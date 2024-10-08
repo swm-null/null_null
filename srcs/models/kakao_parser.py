f9b1cb9d5cfaf825c8fab799a59f65571bfa6792
r4pidstart <r4pidstart@gmail.com>
2024-10-09 08:32:23 +0900

    added: post method kakao-parser

--- srcs/models/kakao_parser.py
+++ srcs/models/kakao_parser.py
@@ -5,7 +5,7 @@ class kakao_parser_type(Enum):
     CSV="csv"
     TXT="txt"
 
-class Arg_kakao_parser(BaseModel):
+class Body_post_kakao_parser(BaseModel):
     user_id: str
     type: kakao_parser_type
     content: str
