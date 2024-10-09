f0bb4af8ed385b60487ab343ad812c593a0325e9
r4pidstart <r4pidstart@gmail.com>
2024-10-09 09:10:32 +0900

    modified: refactored extract and assign metadata

--- srcs/ai/saving/structure/utils/metadata_extractor/metadata_extractor.py
+++ srcs/ai/saving/structure/utils/metadata_extractor/metadata_extractor.py
@@ -1,7 +1,7 @@
 import asyncio
 from ai.saving.structure._models.memo import Memo
-from ai.saving.structure.utils.metadata_extractor.chains.metadata_extractor_chain import metadata_extractor_chain, metadata_extractor_chain_output
-from ai.saving.utils import image_to_text
+from ai.saving.utils import image_to_text, extract_metadata
+from ai.saving.utils.chains.metadata_extractor_chain import Metadata_extractor_chain_output
 
 
 async def extract_and_assign_metadata(memos: dict[int, Memo], lang: str) -> dict[int, Memo]:
@@ -10,14 +10,16 @@ async def extract_and_assign_metadata(memos: dict[int, Memo], lang: str) -> dict
     return dict(await asyncio.gather(*tasks))
         
 async def _extract_and_assign_metadata(id: int, memo: Memo, lang: str) -> tuple[int, Memo]:
-    chain_result: metadata_extractor_chain_output=await metadata_extractor_chain.ainvoke({"content": memo.content, "lang": lang})
+    extract_metadata_task = asyncio.to_thread(extract_metadata, memo.content, lang)
     image_to_text_tasks=[asyncio.to_thread(image_to_text, image) for image in memo.image_urls]
+    
+    extracted_metadata: list[Metadata_extractor_chain_output]=await asyncio.gather(extract_metadata_task)  # type: ignore
     image_to_texts: list[str]=await asyncio.gather(*image_to_text_tasks)
     
     return id, Memo(
         content=memo.content,
         image_urls=memo.image_urls,
-        metadata=chain_result.metadata + ''.join(f"\n{image_to_text}" for image_to_text in image_to_texts),
+        metadata=extracted_metadata[0].metadata + ''.join(f"\n{image_to_text}" for image_to_text in image_to_texts),
         parent_tag_ids=memo.parent_tag_ids,
         timestamp=memo.timestamp
     )
