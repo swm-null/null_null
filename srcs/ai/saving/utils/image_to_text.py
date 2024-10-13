fbfa7f0472a67669d7f38b46a03c4d149b151d17
r4pidstart <r4pidstart@gmail.com>
2024-10-13 21:13:14 +0900

    modified: use user's lang to summarize images

--- srcs/ai/saving/utils/image_to_text.py
+++ srcs/ai/saving/utils/image_to_text.py
@@ -2,12 +2,12 @@ from ai.utils.llm import llm4o
 from langchain_core.messages import HumanMessage
 
 
-def image_to_text(url: str) -> str:
+def image_to_text(url: str, lang: str) -> str:
     result=llm4o.invoke(
         [
             HumanMessage(
                 content=[
-                    {"type": "text", "text": "Summarize the image for me. If there is text, please add the OCR of the original text to the image description."},
+                    {"type": "text", "text": f"Summarize the image for me. If there is text, please add the OCR of the original text to the image description. Summarize it in {lang}."},
                     {"type": "image_url", "image_url": {"url": url}}
                 ]
             )
