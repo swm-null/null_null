d7e9c2f86556d607a68a4bcf2b29cfb725af57b9
r4pidstart <r4pidstart@gmail.com>
2024-10-13 21:39:20 +0900

    modified: use only texts when extracting link's metadata

--- srcs/ai/saving/utils/link_content_fetcher.py
+++ srcs/ai/saving/utils/link_content_fetcher.py
@@ -1,14 +1,20 @@
 import asyncio
 from typing import Optional
 import aiohttp
+import trafilatura
 
 
 async def get_contents_from_link(links: list[str]) -> list[str]:
     async with aiohttp.ClientSession() as session:
         fetch_tasks=[_fetch(session, link) for link in links]
         fetched_results=await asyncio.gather(*fetch_tasks)
-        
-    return [result for result in fetched_results if result]
+    
+    texts=[
+        trafilatura.extract(fetched_result)
+        for fetched_result in fetched_results if fetched_result
+    ]
+    
+    return [text for text in texts if text]
 
 async def _fetch(session, link: str) -> Optional[str]:
     async with session.get(link) as response:
