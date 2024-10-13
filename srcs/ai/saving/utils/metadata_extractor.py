cf71c3d7813b065ce0559e0c27213a805e962d21
r4pidstart <r4pidstart@gmail.com>
2024-10-13 21:14:05 +0900

    modified: return type of metadata extractor chain

--- srcs/ai/saving/utils/metadata_extractor.py
+++ srcs/ai/saving/utils/metadata_extractor.py
@@ -1,8 +1,8 @@
-from ai.saving.utils.chains.metadata_extractor_chain import Metadata_extractor_chain_output, metadata_extractor_chain
+from ai.saving.utils.chains.metadata_extractor_chain import metadata_extractor_chain
 
 
 async def aextract_metadata(content: str, lang: str="Korean"):
     return metadata_extractor_chain.ainvoke({"content": content, "lang": lang})
     
-def extract_metadata(content: str, lang: str="Korean") -> Metadata_extractor_chain_output:
+def extract_metadata(content: str, lang: str="Korean") -> str:
     return metadata_extractor_chain.invoke({"content": content, "lang": lang})
