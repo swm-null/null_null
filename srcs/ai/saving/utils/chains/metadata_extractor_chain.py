9ddccc1cc4ab20ef3684940d7cc54d8b15377de8
r4pidstart <r4pidstart@gmail.com>
2024-10-15 16:09:15 +0900

    modified: prompt to create only descriptions

--- srcs/ai/saving/utils/chains/metadata_extractor_chain.py
+++ srcs/ai/saving/utils/chains/metadata_extractor_chain.py
@@ -8,15 +8,15 @@ _parser = StrOutputParser()
 
 _metadata_extractor_chain_prompt=PromptTemplate.from_template(
 """
-I want to categorize these notes later, or create metadata about them so I can find them easily.
+I want to create description about memo so I can find them easily.
 
 Describe what this note is about, or what it contains, as best you can given the information you have.
-Write the metadata in the user's language as well.
+Write the description in the user's language as well.
 
 You might want to include "주민등록번호" in the metadata if it comes in as a string like "928173-2819811", like a Korean social security number,
 If the note is about a series of addresses, we don't know where, but we can add that this is a note about addresses.
 
-User's language: {lang}              
+User's language: {lang}
 Memo's content: {content}
 """
 )
