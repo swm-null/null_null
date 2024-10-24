72dfc3ee5ef1c38eb11eb1311b58934e60f034b5
r4pidstart <r4pidstart@gmail.com>
2024-10-24 22:29:19 +0900

    added: tag to endpoints

--- srcs/main.py
+++ srcs/main.py
@@ -22,15 +22,15 @@ init(app)
 async def default():
     return "yes. it works."
 
-@app.post("/search", response_model=Res_post_search)
+@app.post("/search", response_model=Res_post_search, tags=["search"])
 async def post_search(body: Arg_post_search):
     return await search_memo(body.content, body.user_id)
 
-@app.post("/get-embedding", response_model=Res_get_embedding)
+@app.post("/get-embedding", response_model=Res_get_embedding, tags=["embedding"])
 def get_embedding(body: Arg_get_embedding):
     return Res_get_embedding(embedding=embedder.embed_query(body.content))
 
-@app.post("/get-metadata-with-embedding", response_model=Res_get_metadata_with_embedding)
+@app.post("/get-metadata-with-embedding", response_model=Res_get_metadata_with_embedding, tags=["embedding"])
 async def post_get_metadata_with_embedding(body: Body_get_metadata_with_embedding):
     metadata=await process_metadata(body.content, body.image_urls)
     
@@ -39,15 +39,15 @@ async def post_get_metadata_with_embedding(body: Body_get_metadata_with_embeddin
         embedding_metadata=await embedder.aembed_query(metadata)
     )
 
-@app.post("/memo/tags", response_model=Res_post_memo_tags)
+@app.post("/memo/tags", response_model=Res_post_memo_tags, tags=["memo"])
 async def post_memo_tags(body: Body_post_memo_tags):
     return Res_post_memo_tags(tags=await create_tags(body.user_id, body.raw_memos))
 
-@app.post("/memo/tag", response_model=Res_post_memo_tag)
+@app.post("/memo/tag", response_model=Res_post_memo_tag, tags=["memo"])
 async def post_memo_tag(body: Body_post_memo_tag):
     return Res_post_memo_tag(tags=await create_tag(body.user_id, body.raw_memo))
 
-@app.post("/memo/structures", response_model=Res_post_memo_structures)
+@app.post("/memo/structures", response_model=Res_post_memo_structures, tags=["memo"])
 async def post_memo_structures(body: Body_post_memo_structures):
     processed_memos, relations, tags=await process_memos(body.user_id, body.memos)
     structure, reversed_structure=await get_structure(body.user_id, relations)
@@ -59,7 +59,7 @@ async def post_memo_structures(body: Body_post_memo_structures):
         new_reversed_structure=reversed_structure
     )
 
-@app.post("/kakao-parser", response_model=Res_post_memo_structures)
+@app.post("/kakao-parser", response_model=Res_post_memo_structures, tags=["parser"])
 async def post_kakao_parser(body: Body_post_kakao_parser):
     parsed_contents: list[tuple[str, datetime]]=await kakao_parser(content=body.content, type=body.type)
     raw_memos: list[Memo_raw_memo]=[
