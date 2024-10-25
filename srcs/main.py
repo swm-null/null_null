7a6230180ae0d7ae7015268fd5646becc8bb9972
r4pidstart <r4pidstart@gmail.com>
2024-10-25 23:58:45 +0900

    modified: version information

--- srcs/main.py
+++ srcs/main.py
@@ -5,7 +5,7 @@ from routers import embedding, importer, memo, search
 
 app = FastAPI(
     title="Oatnote AI",
-    description="after PR #92, https://github.com/swm-null/null_null/pull/91",
+    description="after PR #92, https://github.com/swm-null/null_null/pull/92",
     version="0.2.42",
 )
 init(app)
