bbd4c6dce8e62c1c62ed9ba083abad1a20fca20a
r4pidstart <r4pidstart@gmail.com>
2024-10-29 04:04:24 +0900

    modified: tags test

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -1,4 +1,5 @@
 import asyncio
+import textwrap
 import pytest
 from httpx import ASGITransport, AsyncClient
 from main import app
@@ -9,21 +10,22 @@ body={
     "user_id": "ccc55530-12ed-4a54-b420-025009c0509a",
     "raw_memos": [
         {
-            "content": """
-5GX 플래티넘(넷플릭스)
-무제한
-테더링/공유 120GB 집/이동전화 무제한, 영상/부가통화 300분 문자 기본 제공
-T 우주 Netflix
-넷플릭스 프리미엄 제공
-T 멤버십
-T 멤버십 VIP 혜택
-스마트기기 이용요금
-스마트기기 2회선 이용요금 무료
-월 125,000원
-선택약정 반영 시 93,705원
+            "content": textwrap.dedent("""
+                5GX 플래티넘(넷플릭스)
+                무제한
+                테더링/공유 120GB 집/이동전화 무제한, 영상/부가통화 300분 문자 기본 제공
+                T 우주 Netflix
+                넷플릭스 프리미엄 제공
+                T 멤버십
+                T 멤버십 VIP 혜택
+                스마트기기 이용요금
+                스마트기기 2회선 이용요금 무료
+                월 125,000원
+                선택약정 반영 시 93,705원
 
-https://www.tworld.co.kr/web/product/callplan/NA00008719
-https://asdf.com/this_is_invalid_link""",
+                https://www.tworld.co.kr/web/product/callplan/NA00008719
+                https://asdf.com/this_is_invalid_link
+            """),
             "images": [
                 "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
             ],
