c449e02f266ccb755b270c24051fa7b07f932cb1
r4pidstart <r4pidstart@gmail.com>
2024-11-06 06:18:34 +0900

    modified: import path

--- srcs/ai/memo/tag/utils/chains/determine_tag_names.py
+++ srcs/ai/memo/tag/utils/chains/determine_tag_names.py
@@ -4,7 +4,7 @@ from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 from ai.utils import llm4o_mini
 from langchain_core.prompts import PromptTemplate
-from ai.memo.tag.deprecated._configs import TAG_SELECTION_COUNT
+from ai.memo.tag._configs import TAG_SELECTION_COUNT
 
 
 class _Determine_tag_names_chain_input(BaseModel):
