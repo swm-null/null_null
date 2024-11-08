360f8b9b2e8bd966bccb703b1055e674167fb2dc
r4pidstart <r4pidstart@gmail.com>
2024-11-08 23:52:17 +0900

    modified: use keyword search result on search using ai

--- srcs/ai/search/ai_search/utils/similarity/utils/agents/tools/memo_answerer.py
+++ srcs/ai/search/ai_search/utils/similarity/utils/agents/tools/memo_answerer.py
@@ -3,7 +3,7 @@ from langchain_core.tools import tool
 from openai import BaseModel
 from pydantic import Field
 from ai.search._models.memo import Memo
-from ai.search.ai_search.utils.similarity.utils.retrieve_similar_memos_from_db import retrieve_similar_memos_from_db
+from ai.search.ai_search.utils.similarity.utils.retrieve_similar_memos_from_db import retrieve_similar_memos_from_db, aretrieve_similar_memos_from_db
 from ai.search.ai_search.utils.similarity.utils.chains.similarity_result_with_memo_chain import asimilarity_result_with_memo, similarity_result_with_memo, Similarity_result_with_memo_chain_output
 from fastapi.concurrency import run_in_threadpool
 
@@ -14,14 +14,14 @@ class _memo_answer_args(BaseModel):
     language: str=Field(description="user's language")
 
 @tool("memo_answerer", args_schema=_memo_answer_args, return_direct=False)
-def memo_answerer(user_id: str, question: str, start_time: datetime, end_time: datetime, language: str) -> Similarity_result_with_memo_chain_output:
+def memo_answerer(user_id: str, question: str, keyword: str, start_time: datetime, end_time: datetime, language: str) -> Similarity_result_with_memo_chain_output:
     """Use the user's memos to check if the given question can be answered, and if so, obtain the answer."""
-    retrieved_memos: list[Memo]=retrieve_similar_memos_from_db(question, user_id, start_time, end_time)
+    retrieved_memos: list[Memo]=retrieve_similar_memos_from_db(question, keyword, user_id, start_time, end_time)
     
     return similarity_result_with_memo(question, retrieved_memos, language)
 
-async def amemo_answerer(user_id: str, question: str, start_time: datetime, end_time: datetime, language: str) -> Similarity_result_with_memo_chain_output:
+async def amemo_answerer(user_id: str, question: str, keyword: str, start_time: datetime, end_time: datetime, language: str) -> Similarity_result_with_memo_chain_output:
     """Use the user's memos to check if the given question can be answered, and if so, obtain the answer."""
-    retrieved_memos: list[Memo]=await run_in_threadpool(retrieve_similar_memos_from_db, question, user_id, start_time, end_time)
+    retrieved_memos: list[Memo]=await aretrieve_similar_memos_from_db(question, keyword, user_id, start_time, end_time)
     
     return await asimilarity_result_with_memo(question, retrieved_memos, language)
