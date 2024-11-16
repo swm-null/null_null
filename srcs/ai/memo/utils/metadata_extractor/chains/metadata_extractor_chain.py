86cb987ff83c4a1851611b7bfdd7bda7c3da036e
r4pidstart <r4pidstart@gmail.com>
2024-11-17 02:32:01 +0900

    modified: prompt

--- srcs/ai/memo/utils/metadata_extractor/chains/metadata_extractor_chain.py
+++ srcs/ai/memo/utils/metadata_extractor/chains/metadata_extractor_chain.py
@@ -38,6 +38,7 @@ _metadata_extractor_chain_prompt=PromptTemplate.from_template(textwrap.dedent(""
     The user may need this memo to find some information later. Extract keywords for that purpose.
     Think about what keywords the user might use to find this memo.
     For example, if this memo is related to a schedule, you can select 'schedule' as a keyword.
+    Extract keywords so that users can search for similar terms later. For example, so that 'to-do' can be found by searching for 'schedule'.
 
     3. Extract and convert words that indicate relative time.
     memos such as schedules may use words that indicate relative time, such as "next week." These should be converted based on the current time so that the user can find them later.
